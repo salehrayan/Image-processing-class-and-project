4fbb265eb3ae19df1d2687888c35d056681cbfb4
salehrayan <138001028+salehrayan@users.noreply.github.com>
2023-11-10 02:39:19 +0330

    u

--- Image restoration/Image restoration.py
+++ Image restoration/Image restoration.py
@@ -5,8 +5,6 @@ from scipy import signal
 import scipy
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from skimage import restoration
-from Histogram_matching import histogram_match_fn
 
 
 np.random.seed(42)
@@ -34,7 +32,12 @@ def noise_remove(file_path, intensity_factor, std, blurring_kernel_dim, MF_dim ,
     kernel_average = np.ones((Mean_F_dim, Mean_F_dim))/ (Mean_F_dim**2)
 
     image_blur = signal.correlate2d(image, kernel_blur, boundary="symm")
-    image_blur_noise = image_blur + std* np.random.randn(image_blur.shape[0], image_blur.shape[1])
+    noise = std* np.random.randn(image_blur.shape[0], image_blur.shape[1])
+    image_blur_noise = image_blur + noise
+
+    snr = np.sum(image**2)/np.sum(np.abs(noise[blurring_kernel_dim-1:noise.shape[0]-(blurring_kernel_dim-2),
+                               blurring_kernel_dim-1:noise.shape[1]-(blurring_kernel_dim-2)])**2)
+    print(snr)
 
     kernel_blur_fft = np.fft.fft2(kernel_blur, s=(image_blur_noise.shape[0], image_blur_noise.shape[1]))
     image_blur_noise_fft = np.fft.fft2(image_blur_noise, s=(image_blur_noise.shape[0], image_blur_noise.shape[1]))
@@ -48,23 +51,23 @@ def noise_remove(file_path, intensity_factor, std, blurring_kernel_dim, MF_dim ,
     rec_image2 = signal.correlate2d(image_blur_noise, kernel_average, boundary='symm')
 
     rec_image_k1 = np.abs(
-        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 1)) / kernel_blur_fft) *
+        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 1)) / (kernel_blur_fft+0.01)) *
                      image_blur_noise_fft))
     rec_image_k1 = (rec_image_k1 - np.min(rec_image_k1))/(np.max(rec_image_k1) - np.min(rec_image_k1)) *255
 
     rec_image_kpoint1 = np.abs(
-        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 0.1)) / kernel_blur_fft) *
+        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 0.1)) / (kernel_blur_fft+0.01)) *
                      image_blur_noise_fft))
     rec_image_kpoint1 = (rec_image_kpoint1 - np.min(rec_image_kpoint1)) / (np.max(rec_image_kpoint1) - np.min(rec_image_kpoint1)) * 255
 
     rec_image_kpoint5 = np.abs(
-        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 0.5)) / kernel_blur_fft) *
+        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 0.5)) / (kernel_blur_fft+0.01)) *
                      image_blur_noise_fft))
     rec_image_kpoint5 = (rec_image_kpoint5 - np.min(rec_image_kpoint5)) / (
                 np.max(rec_image_kpoint5) - np.min(rec_image_kpoint5)) * 255
 
     rec_image_k10 = np.abs(
-        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 10)) / kernel_blur_fft) *
+        np.fft.ifft2((((np.abs(kernel_blur_fft) ** 2) / (np.abs(kernel_blur_fft) ** 2 + 10)) / (kernel_blur_fft+0.01)) *
                      image_blur_noise_fft))
     rec_image_k10 = (rec_image_k10 - np.min(rec_image_k10)) / (
             np.max(rec_image_k10) - np.min(rec_image_k10)) * 255
@@ -72,6 +75,7 @@ def noise_remove(file_path, intensity_factor, std, blurring_kernel_dim, MF_dim ,
     image_blur_noise_padded = np.pad(image_blur_noise, ((MF_dim-2, MF_dim-2), (MF_dim-2, MF_dim-2)), mode="symmetric")
     rec_image = image_blur_noise_padded.copy()
     rec_image3 = image_blur_noise_padded.copy()
+
     for row in tqdm(range(MF_dim-2,image_blur_noise.shape[0]+(MF_dim-2))):
         for col in range(MF_dim-2, image_blur_noise.shape[1]+(MF_dim-2)):
 
@@ -81,15 +85,9 @@ def noise_remove(file_path, intensity_factor, std, blurring_kernel_dim, MF_dim ,
             '''Wiener Filter'''
             mu = np.mean(temp)
             sigma2 = np.var(temp)
-            rec_image3[row, col] = mu + ((sigma2 - std**2)/sigma2)*(image_blur_noise_padded[row, col]-mu)
+            rec_image3[row, col] = mu + ((sigma2 - std**2)/(sigma2 + 0.01))*(image_blur_noise_padded[row, col]-mu)
 
-
-
-
-
-
-
-    fig1 = plt.figure(figsize=(8, 9))
+    plt.figure(figsize=(8, 10))
 
     ax1 = plt.subplot(3, 2, 1)
     plt.imshow(image, cmap='gray', vmax=255, vmin=0)
@@ -117,7 +115,7 @@ def noise_remove(file_path, intensity_factor, std, blurring_kernel_dim, MF_dim ,
 
     plt.tight_layout()
 
-    fig2 = plt.figure(figsize=(4, 9))
+    plt.figure(figsize=(4, 10))
 
     ax7 = plt.subplot(3, 1, 1, sharex=ax1, sharey=ax1)
     plt.imshow(rec_image_kpoint1+np.abs(np.mean(rec_image_kpoint1) - np.mean(rec_image2)), cmap='gray', vmax=255, vmin=0)
@@ -134,10 +132,12 @@ def noise_remove(file_path, intensity_factor, std, blurring_kernel_dim, MF_dim ,
     plt.title(f'Full Wiener filtered, K=10', fontname='Times New Roman', fontweight="bold")
 
     plt.tight_layout()
+
     plt.show()
+
 
 
 if __name__ == '__main__':
     file_path = r'C:\Users\ASUS\Desktop\Image processing\Horizontal texture features\textures\1.4.02.tiff'
-    noise_remove(file_path, intensity_factor=1, std=20, blurring_kernel_dim=4,
+    noise_remove(file_path, intensity_factor=1, std=40, blurring_kernel_dim=4,
                  MF_dim=4, Mean_F_dim=4)
--- Inverse fourier transform/playgorund_1.py
+++ Inverse fourier transform/playgorund_1.py
@@ -5,7 +5,10 @@ from scipy import signal
 import scipy
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from skimage import restoration
+
+
+def minmax(array):
+    return (array - np.min(array)) / (np.max(array) - np.min(array)) * 255
 
 
 def inverse_fourier(file1_path, file2_path, intensity_factor):
@@ -16,22 +19,73 @@ def inverse_fourier(file1_path, file2_path, intensity_factor):
     image2_fft = np.fft.fft2(image2)
 
     image1_fft_phaseinfo = image1_fft.copy()
+    image2_fft_phaseinfo = image2_fft.copy()
 
     image1_fft_phaseinfo[np.abs(image1_fft) != 0] = image1_fft_phaseinfo[np.abs(image1_fft) != 0] / \
-        np.abs(image1_fft)[np.abs(image1_fft) != 0]
+                                                    np.abs(image1_fft)[np.abs(image1_fft) != 0]
 
-    plt.imshow(np.abs(np.fft.ifft2(image1_fft_phaseinfo)), cmap="gray")
-    plt.show()
+    image2_fft_phaseinfo[np.abs(image2_fft) != 0] = image2_fft_phaseinfo[np.abs(image2_fft) != 0] / \
+                                                    np.abs(image2_fft)[np.abs(image2_fft) != 0]
 
-    plt.imshow(np.log(np.abs(np.fft.fftshift(image1_fft))+1) ** intensity_factor, cmap="gray")
-    plt.show()
+    im1phase_im2spec = image1_fft_phaseinfo * np.abs(image2_fft)
+    im2phase_im1spec = image2_fft_phaseinfo * np.abs(image1_fft)
 
-    plt.imshow(np.log(np.abs(np.fft.fftshift(image2_fft)) + 1) ** intensity_factor, cmap="gray")
+    fig1 = plt.figure(figsize=(16, 10))
+
+
+    plt.subplot(3, 4, 1)
+    plt.imshow(image1, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 1', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 2)
+    plt.imshow(image2, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 2', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 3)
+    plt.imshow(np.log(np.abs(np.fft.fftshift(image1_fft)) + 1) ** intensity_factor, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 1 Magnitude spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 4)
+    plt.imshow(np.log(np.abs(np.fft.fftshift(image2_fft)) + 1) ** intensity_factor, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 2 Magnitude spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 5)
+    plt.imshow(minmax(np.abs(image1_fft_phaseinfo)), cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 1 Phase spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 6)
+    plt.imshow(minmax(np.abs(image2_fft_phaseinfo)), cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 2 phase spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 7)
+    plt.imshow(minmax(np.abs(np.fft.ifft2(np.abs(image1_fft)))) ** 1.5, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 1 rec. from spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 8)
+    plt.imshow(minmax(np.abs(np.fft.ifft2(np.abs(image2_fft)))) ** 1.5, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 2 rec. from spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 9)
+    plt.imshow(minmax(np.abs(np.fft.ifft2(image1_fft_phaseinfo))) * intensity_factor, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 1 rec. from phase', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 10)
+    plt.imshow(minmax(np.abs(np.fft.ifft2(image2_fft_phaseinfo))) * intensity_factor, cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Image 2 rec. from phase.', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 11)
+    plt.imshow(minmax(np.abs(np.fft.ifft2(im1phase_im2spec))), cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Rec. with Image 1 phase\nand Image 2 mag. spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.subplot(3, 4, 12)
+    plt.imshow(minmax(np.abs(np.fft.ifft2(im2phase_im1spec))), cmap="gray", vmin=0, vmax=255)
+    plt.title(f'Rec. with Image 2 phase\nand Image 1 mag. spectrum', fontname='Times New Roman', fontweight="bold")
+
+    plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
-
-    file1_path = r'C:\Users\ASUS\Desktop\Image processing\Rotate and resize\standard_test_images\lena_gray_512.tif'
-    file2_path = r'C:\Users\ASUS\Desktop\Image processing\Rotate and resize\standard_test_images\woman_darkhair.tif'
-    inverse_fourier(file1_path, file2_path, intensity_factor=1)
+    file1 = r'C:\Users\ASUS\Desktop\Image processing\Rotate and resize\standard_test_images\lena_gray_512.tif'
+    file2 = r'C:\Users\ASUS\Desktop\Image processing\Rotate and resize\standard_test_images\woman_darkhair.tif'
+    inverse_fourier(file1, file2, intensity_factor=2.1)
