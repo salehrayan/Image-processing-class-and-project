038867f663103dd481f4dd41cc8060e9cd204a03
salehrayan <138001028+salehrayan@users.noreply.github.com>
2023-11-14 00:23:03 +0330

    up

--- Inverse fourier transform/Fourier.py
+++ Inverse fourier transform/Fourier.py
@@ -71,19 +71,19 @@ def inverse_fourier(file1_path, file2_path, intensity_factor, T=None):
     plt.title(f'Image 2 rec. from spectrum', fontname='Times New Roman', fontweight="bold")
 
     plt.subplot(3, 4, 9)
-    plt.imshow(minmax(np.abs(np.fft.ifft2(image1_fft_phaseinfo))) * intensity_factor, cmap="gray", vmin=0, vmax=255)
+    plt.imshow(minmax(np.abs(np.real(np.fft.ifft2(image1_fft_phaseinfo)))), cmap="gray", vmin=0, vmax=255)
     plt.title(f'Image 1 rec. from phase', fontname='Times New Roman', fontweight="bold")
 
     plt.subplot(3, 4, 10)
-    plt.imshow(minmax(np.abs(np.fft.ifft2(image2_fft_phaseinfo))) * intensity_factor, cmap="gray", vmin=0, vmax=255)
+    plt.imshow(minmax(np.abs(np.real(np.fft.ifft2(image2_fft_phaseinfo)))), cmap="gray", vmin=0, vmax=255)
     plt.title(f'Image 2 rec. from phase.', fontname='Times New Roman', fontweight="bold")
 
     plt.subplot(3, 4, 11)
-    plt.imshow(minmax(np.real(np.fft.ifft2(im1phase_im2spec))), cmap="gray", vmin=0, vmax=255)
+    plt.imshow(minmax(np.abs(np.real(np.fft.ifft2(im1phase_im2spec)))), cmap="gray", vmin=0, vmax=255)
     plt.title(f'Rec. with Image 1 phase\nand Image 2 mag. spectrum', fontname='Times New Roman', fontweight="bold")
 
     plt.subplot(3, 4, 12)
-    plt.imshow(minmax(np.real(np.fft.ifft2(im2phase_im1spec))), cmap="gray", vmin=0, vmax=255)
+    plt.imshow(minmax(np.abs(np.real(np.fft.ifft2(im2phase_im1spec)))), cmap="gray", vmin=0, vmax=255)
     plt.title(f'Rec. with Image 2 phase\nand Image 1 mag. spectrum', fontname='Times New Roman', fontweight="bold")
 
     plt.tight_layout()
@@ -107,4 +107,4 @@ if __name__ == "__main__":
     image_T[middle - t_c_h: middle + t_c_h, middle - t_c_w: middle + t_c_w] = 255
     image_T[middle - t_c_h: middle - t_c_h + t_h_h, middle - t_h_w: middle + t_h_w] = 255
 
-    inverse_fourier(file1, file2, intensity_factor=2.1, T=image_T)
+    inverse_fourier(file1, file2, intensity_factor=2, T=image_T)
